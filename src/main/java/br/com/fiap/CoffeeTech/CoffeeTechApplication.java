ca3c8eb888dbab5a7a8810def9b7d738813e2e42
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2026-01-25 00:29:48 -0300

    Initial project setup with Spring Boot

---
+++ src/main/java/br/com/fiap/CoffeeTech/CoffeeTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.CoffeeTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeTechApplication.class, args);
+	}
+
+}
